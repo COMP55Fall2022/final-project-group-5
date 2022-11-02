dd068c5e6b1b62e5ef505c930b93d674d9633c37
Talissa Montez <t_montez@u.pacific.edu>
2022-11-02 14:31:33 -0700

    PlayerShip: public methods

--- src/main/java/edu/pacific/comp55/starter/PlayerShip.java
+++ src/main/java/edu/pacific/comp55/starter/PlayerShip.java
@@ -3,4 +3,25 @@ package edu.pacific.comp55.starter;
 public class PlayerShip {
 	private Shot shot;
 	private int lives;
+	
+	public PlayerShip player(int x, int y) {
+		
+		return null;
+	}
+	
+	public void revive() {
+		
+	}
+	
+	public void pause() {
+		
+	}
+	
+	public void resume() {
+		
+	}
+	
+	public void damaged() {
+		
+	}
 }
