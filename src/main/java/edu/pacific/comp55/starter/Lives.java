26b722ddb0ac60bac884aa4940369c0c4e5bd74c
rakeshthalari <rakeshthalari@dhcp-10-15-187-21.wireless.pacific.edu>
2022-11-06 21:37:34 -0800

    lives

--- src/main/java/edu/pacific/comp55/starter/Lives.java
+++ src/main/java/edu/pacific/comp55/starter/Lives.java
@@ -1,46 +1,26 @@
 package edu.pacific.comp55.starter;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
+
+
 
 public class Lives{
-	Text lives;
-	Text points;
-	int numPoints = 0;
-	int numLives = 3;
-	Pane root = new Pane();
- 
-	public static void main(String[] args) {
 	
- 
-	}
-	public void start(Stage primaryStage) throws Exception {
- 
-		//life and points
- 
-		lives = new Text("Lives: 3");
-		lives.setLayoutX(20);
-		lives.setLayoutY(30);
-		lives.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20)); 
-		lives.setFill(Color.WHITE);
-	    points = new Text("Points: 0");
-	    points.setLayoutX(350);
-		points.setLayoutY(30);
-		points.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20)); 
-		points.setFill(Color.WHITE);
-		root.getChildren().addAll(lives, points);
-}
-	private void isPlayerDestroyed() {
-		// write a for loop and if condition on when to decrease the lives
+    public static void main(String[] args) {  
+        //initialize counter  
+        int livesCounter = 4;  
+          
+        //using for loop to decrement the counter variable  
+        for(int lives=0; lives<4; lives++){  
+        	//write the condition when hit by the em\nemy
+            //decrement counter variable  
+            livesCounter = livesCounter - 1;  
+            //print the counter variable value  when shot
+            System.out.println(livesCounter);  
+            if(livesCounter == 0) {
+                System.out.println("Lost");  
 
-		
-					       numLives -= 1;
-					       lives.setText("Lives: " + String.valueOf(numLives));
-	 
-		
-	}
+            }
+        }  
+    }
+	
+	
 }
