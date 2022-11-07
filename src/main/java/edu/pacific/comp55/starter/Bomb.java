b1b8c964731441984f747fa45769955e8d743468
Jason Hoang <j_hoang18@u.pacific.edu>
2022-11-06 22:04:02 -0800

    Bomb

--- src/main/java/edu/pacific/comp55/starter/Bomb.java
+++ src/main/java/edu/pacific/comp55/starter/Bomb.java
@@ -2,8 +2,23 @@ package edu.pacific.comp55.starter;
 
 public class Bomb {
 
+	private Object x;
+	private Object y;
 	public Bomb(int x, int y) {
-		// TODO Auto-generated constructor stub
+		initBomb(x, y);
+	}
+
+	private void initBomb(int x, int y) {
+		this.x;
+		this.y;
+	
+		
+		
+	}
+
+	private void invaders(boolean b) {
+		
+		
 	}
 
 }
