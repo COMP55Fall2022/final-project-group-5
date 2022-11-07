d8649237e50ca06388bb3d0fc8b36477ab944bfc
Talissa Montez <t_montez@u.pacific.edu>
2022-11-06 18:41:11 -0800

    Image for ship

--- src/main/java/edu/pacific/comp55/starter/PlayerShip.java
+++ src/main/java/edu/pacific/comp55/starter/PlayerShip.java
@@ -1,27 +1,32 @@
 package edu.pacific.comp55.starter;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.awt.*;
 
-public class PlayerShip {
+public class PlayerShip  {
 	private Shot shot;
 	private int lives;
+	GImage shipImage = new GImage("SeekPng.com_space-ship-png_254127.png");
 	
 	public PlayerShip player(int x, int y) {
 		
 		return null;
 	}
 	
-	public void revive() {
+	public void revive() { 
 		
 	}
 	
-	public void pause() {
+	public void pause() { //connect to the pause button
 		
 	}
 	
-	public void resume() {
+	public void resume() { //connect to resume button
 		
 	}
 	
-	public void damaged() {
+	public void damaged() { 
 		
 	}
 }
