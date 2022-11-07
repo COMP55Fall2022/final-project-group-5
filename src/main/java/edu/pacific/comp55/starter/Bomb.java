6ec28e25c977d8af90bed547ad3432b0184273c3
Jason Hoang <j_hoang18@u.pacific.edu>
2022-11-06 22:06:30 -0800

    more bomb

--- src/main/java/edu/pacific/comp55/starter/Bomb.java
+++ src/main/java/edu/pacific/comp55/starter/Bomb.java
@@ -2,23 +2,25 @@ package edu.pacific.comp55.starter;
 
 public class Bomb {
 
-	private Object x;
-	private Object y;
+	private int x;
+	private int y;
+	private boolean invaders;
 	public Bomb(int x, int y) {
-		initBomb(x, y);
+		 initBomb(x , y);
 	}
-
 	private void initBomb(int x, int y) {
-		this.x;
-		this.y;
-	
-		
-		
+		this.x = x;
+		this.y = y;
+		invaders(true);
+	//var imgBomb = ""
+	//var II = new "";
+	//setImage(ii.getImage());	
 	}
-
 	private void invaders(boolean b) {
 		
+	}
+		
 		
 	}
 
-}
+
