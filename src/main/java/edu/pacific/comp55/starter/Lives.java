df85ab39defb0d74ddfa21b3f6f839d39d71a9b1
rakeshthalari <rakeshthalari@dhcp-10-15-187-21.wireless.pacific.edu>
2022-11-09 14:16:43 -0800

    n

--- src/main/java/edu/pacific/comp55/starter/Lives.java
+++ src/main/java/edu/pacific/comp55/starter/Lives.java
@@ -10,7 +10,7 @@ public class Lives{
           
         //using for loop to decrement the counter variable  
         for(int lives=0; lives<4; lives++){  
-        	//write the condition when hit by the em\nemy
+        	//write the condition when hit by the enemy
             //decrement counter variable  
             livesCounter = livesCounter - 1;  
             //print the counter variable value  when shot
