b03cb1ffd56023f3a6d4ce3071c2e7896ca49de1
rakeshthalari <rakeshthalari@dhcp-10-15-187-21.wireless.pacific.edu>
2022-11-12 11:07:21 -0800

    lives

--- src/main/java/edu/pacific/comp55/starter/Lives.java
+++ src/main/java/edu/pacific/comp55/starter/Lives.java
@@ -4,24 +4,18 @@ package edu.pacific.comp55.starter;
 
 public class Lives{
 	
-    public static void main(String[] args) {  
-        //initialize counter  
-        int livesCounter = 4;  
-          
-        //using for loop to decrement the counter variable  
-        for(int lives=0; lives<4; lives++){  
-        	//write the condition when hit by the enemy
-            //decrement counter variable  
-            livesCounter = livesCounter - 1;  
-            //print the counter variable value  when shot
-            System.out.println(livesCounter);  
-            if(livesCounter == 0) {
-                System.out.println("Lost");  
-
-            }
-        }  
-    }
+	static int count(int livesCounter) {
+		  livesCounter = livesCounter - 1; 
+		   
+		  if(livesCounter == -1) {
+		    System.out.println("Lost");  
+		  }
+		 
+		  
+		  }
 	
 	
 }
-//new 13
+
+
+   
