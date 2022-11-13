ea016edb90cde332155cbb3d534183d64efcbfd2
rakeshthalari <rakeshthalari@dhcp-10-15-187-21.wireless.pacific.edu>
2022-11-12 22:40:08 -0800

    adding 2 counters

--- src/main/java/edu/pacific/comp55/starter/Lives.java
+++ src/main/java/edu/pacific/comp55/starter/Lives.java
@@ -1,12 +1,11 @@
 package edu.pacific.comp55.starter;
 
-
+import acm.graphics.GImage;
 
 public class Lives{
 	
 	static int count(int livesCounter) {
 		  livesCounter = livesCounter - 1; 
-		   
 		  if(livesCounter == -1) {
 		    System.out.println("Lost");  
 		  }
@@ -16,7 +15,14 @@ public class Lives{
 		  }
 		  return livesCounter;
 		}
-
+	public static void main(String[] args) {
+	    int livesCounter = 3;  
+	  livesCounter=count(livesCounter);
+	  livesCounter=count(livesCounter);
+	 
+	  
+	  
+	  }
 		  
 	
 	
