7c582b8f06cbc0c495fb2b3fbc92d8a7910fcd8c
Talissa Montez <t_montez@u.pacific.edu>
2022-11-13 22:05:57 -0800

    update shot image

--- src/main/java/edu/pacific/comp55/starter/Shot.java
+++ src/main/java/edu/pacific/comp55/starter/Shot.java
@@ -4,16 +4,41 @@ import acm.program.*;
 import acm.util.*;
 import java.awt.*;
 
-public class Shot {
+public class Shot extends GraphicsProgram {
+	GObject shot = new GImage("images/shot1.png");
+	int x;
+	int y;
+	int START_X;
+	int START_Y;
+	
+	private void setX(int x) {
+		this.x = x;
+	}
+	
+	private void setY(int y) {
+		this.y = y;
+	}
+	
 	private void removeShot() {
 		
 	}
 	
-	public Shot(int x, int y) {
-		fireShot(x,y);
+	public Shot() {
+		fireShot();
 	}
 	
-	public void fireShot(int x, int y) {
-		GObject shot;
+	@Override
+	public void run() {
+		add(shot);
+		
+		addKeyListeners();
+	}
+	
+	public void fireShot() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new Shot().start();
 	}
 }
