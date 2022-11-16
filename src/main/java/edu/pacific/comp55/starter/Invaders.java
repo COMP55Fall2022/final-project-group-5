4963ef4737a3f7e87938df96a9c91dfb80a2cc88
Jason Hoang <j_hoang18@u.pacific.edu>
2022-11-16 14:22:28 -0800

    won't compile and run but has no errors

--- src/main/java/edu/pacific/comp55/starter/Invaders.java
+++ src/main/java/edu/pacific/comp55/starter/Invaders.java
@@ -1,27 +1,36 @@
 package edu.pacific.comp55.starter;
 
 import acm.graphics.GImage;
+import acm.graphics.GObject;
+import acm.program.GraphicsProgram;
 
 
-public class Invaders {
+public class Invaders extends GraphicsProgram {
 	private Bomb bomb;
 	private int x;
 	private int y;
-	public Invaders(int x, int y) {
+	GObject invaders = new GImage("media/invaders(test).PNG");
+	public Invaders() {
 		initInvaders(x, y);
 	}
 	private void initInvaders(int x, int y) {
 		this.x = x;
 		this.y = y;
-		
-		GImage invaders = new GImage("media/invaders.png");
 		bomb = new Bomb(x, y);
 	}
 	public void act(int direction) {
 		this.x += direction;
 	}
 	public Bomb getBomb() {
-		return bomb;
+		return bomb;	}
+	
+	@Override
+	public void run() {
+		add(invaders);
+		
+	}
+	public static void main (String[] args) {
+		new Invaders().start();
 	}
 }
 		
