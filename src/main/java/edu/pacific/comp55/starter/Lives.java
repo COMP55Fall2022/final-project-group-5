06e7193765f5c4170d411a6d8e80f667fce3ff1f
rakeshthalari <rakeshthalari@localhost>
2022-11-16 14:32:36 -0800

    lives

--- src/main/java/edu/pacific/comp55/starter/Lives.java
+++ src/main/java/edu/pacific/comp55/starter/Lives.java
@@ -1,8 +1,30 @@
 package edu.pacific.comp55.starter;
 
-import acm.graphics.GImage;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.ArrayList;
 
-public class Lives{
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+import acm.program.GraphicsProgram;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class Lives extends GraphicsProgram {
+	GraphicsProgram gamescreenRef;
+	int START_X = 10;
+	int START_Y = 10;
+	GObject livesImage = new GImage("images/playerShip1.png", START_X, START_Y);
+	private ArrayList<GImage>Lives;
+	Lives(GraphicsProgram Ref) {
+		gamescreenRef = Ref;
+	}
+	
+	public void Draw() {
+		//GImage Live1 = new GImage("images/lives1.png", )
+	}
 	
 	static int count(int livesCounter) {
 		  livesCounter = livesCounter - 1; 
@@ -15,19 +37,27 @@ public class Lives{
 		  }
 		  return livesCounter;
 		}
+
 	public static void main(String[] args) {
-	    int livesCounter = 3;  
+	   
+	  //new Lives().start();
+	  int livesCounter = 3;  
 	  livesCounter=count(livesCounter);
 	  livesCounter=count(livesCounter);
 	  livesCounter=count(livesCounter);
 	  livesCounter=count(livesCounter);
-	  
 	  
 	  }
+	
+	public void run() {
+		// TODO Auto-generated method stub
+		add(livesImage);
+	
+		
+		
+	}
+	
 		  
 	
 	
 }
-
-
-   
