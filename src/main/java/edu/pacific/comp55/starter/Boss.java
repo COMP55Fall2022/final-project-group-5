565b096a8831a1863e7b7ced4e5496b01f7e9885
rakeshthalari <rakeshthalari@dhcp-10-15-187-21.wireless.pacific.edu>
2022-11-30 00:11:55 -0800

    boss

--- src/main/java/edu/pacific/comp55/starter/Boss.java
+++ src/main/java/edu/pacific/comp55/starter/Boss.java
@@ -1,5 +1,57 @@
 package edu.pacific.comp55.starter;
 
-public class Boss {
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+import acm.program.GraphicsProgram;
 
+
+public class Boss {
+	private Bomb bomb;
+	int x;
+	int y;
+	int start_x = 50;
+	int start_y = 50;
+	GObject Boss = new GImage("media/new invaders boss2.png", start_x, start_y);
+	GraphicsProgram gameScr; 
+	public Boss(GraphicsProgram screen) {
+		gameScr = screen;
+		gameScr.add(Boss);
+	}
+	
+	public Boss() {
+		initInvaders();
+	}
+	private void initInvaders() {
+		setX(start_x);
+		setY(start_y);
+		//bomb = new Bomb(x, y);
+	}
+	private void setX(int x) {
+		this.x = x;
+	}
+	private void setY(int y) {
+		this.y = y;
+	}
+	
+	public double getX() {
+		return Boss.getX();
+	}
+	
+	public double getY() {
+		return Boss.getY();
+	}
+	
+	public Bomb getBomb() {
+		return bomb;	
+		}
+	
+	public void run() {
+		gameScr.add(Boss);
+	}
 }
+	
+		
+	
+
+
+
