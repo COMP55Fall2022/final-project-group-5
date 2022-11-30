e792a8096b6dab2247fffa0d5a439845595a6c21
rakeshthalari <rakeshthalari@dhcp-10-15-187-21.wireless.pacific.edu>
2022-11-30 09:51:46 -0800

    boss

--- src/main/java/edu/pacific/comp55/starter/Boss.java
+++ src/main/java/edu/pacific/comp55/starter/Boss.java
@@ -9,49 +9,15 @@ public class Boss {
 	private Bomb bomb;
 	int x;
 	int y;
-	int start_x = 50;
-	int start_y = 50;
+	int start_x = 250;
+	int start_y = 250;
 	GObject Boss = new GImage("media/new invaders boss2.png", start_x, start_y);
 	GraphicsProgram gameScr; 
+	
+	
+	
 	public Boss(GraphicsProgram screen) {
 		gameScr = screen;
 		gameScr.add(Boss);
 	}
-	
-	public Boss() {
-		initInvaders();
 	}
-	private void initInvaders() {
-		setX(start_x);
-		setY(start_y);
-		//bomb = new Bomb(x, y);
-	}
-	private void setX(int x) {
-		this.x = x;
-	}
-	private void setY(int y) {
-		this.y = y;
-	}
-	
-	public double getX() {
-		return Boss.getX();
-	}
-	
-	public double getY() {
-		return Boss.getY();
-	}
-	
-	public Bomb getBomb() {
-		return bomb;	
-		}
-	
-	public void run() {
-		gameScr.add(Boss);
-	}
-}
-	
-		
-	
-
-
-
